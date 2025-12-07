497aae825901defb25e43ec543548024f565a23a
Jue Chemparathy <juchemp@amazon.com>
2025-12-06 20:14:34 -0800

    updated UI

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // Get these from Firebase console → Project settings → Web app
 const firebaseConfig = {
-    apiKey: "AIzaSyAOWfR3KGG-ouX6Whl27lApxHMukZ2G9UE",
-    authDomain: "svsprouts-a1350.firebaseapp.com",
-    projectId: "svsprouts-a1350",
-    storageBucket: "svsprouts-a1350.firebasestorage.app",
-    messagingSenderId: "575688818236",
-    appId: "1:575688818236:web:648234b064245b9bf10d39",
-    measurementId: "G-6PTY3QCH6H"
+  apiKey: "AIzaSyAOWfR3KGG-ouX6Whl27lApxHMukZ2G9UE",
+  authDomain: "svsprouts-a1350.firebaseapp.com",
+  projectId: "svsprouts-a1350",
+  storageBucket: "svsprouts-a1350.firebasestorage.app",
+  messagingSenderId: "575688818236",
+  appId: "1:575688818236:web:648234b064245b9bf10d39",
+  measurementId: "G-6PTY3QCH6H"
 };
 
 firebase.initializeApp(firebaseConfig);
